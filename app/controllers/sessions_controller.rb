e2882985e1eaf738dec6f53c8779731337ed3bd6
Esperanza11 <eruribe@miuandes.cl>
2018-04-17 14:35:06 -0300

    now you can log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,24 @@ class SessionsController < ApplicationController
 
 	def create
 		#complete this method
-		user = User.where(email: params[:email])
-		if user.password == params[password]
+		user = User.find_by_email(user_params[:email])
+		if user.password == user_params[:password]
 			session[:current_user_id] = user.id
 		end
 		if is_user_logged_in?
-			redirect_to "/users/%s"%[current_user.id]
 			flash[:notice] = "success"
+			redirect_to :controller => :users, :action => :show, :id => user.id
 		else
-			redirect_to root_url
 			flash[:notice] = "error"
+			redirect_to root_url
 		end
 	end
 
 	def destroy
 		#complete this method
+	end
+private
+	def user_params
+		params.require(:session).permit(:email, :password)
 	end
 end
